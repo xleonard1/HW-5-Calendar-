3a74e4e2ef1cd3153c78fce26077972418b23757
xleonard1 <xavier.lnrd@gmail.com>
2022-04-05 23:25:46 -0400

    test commit

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,14 @@
-var dayDisplayEl = $('#currentDay')
-var timeDisplayEl = $('#currentTime')
-var saveButtonEl = $('.saveBtn')
-var textArea = $('.textarea')
-var p = $('p')
+var dayDisplayEl = $('#currentDay');
+var timeDisplayEl = $('#currentTime');
+var saveButtonEl = $('.saveBtn');
+var textArea = $('.textarea');
+var p = $('p');
 // handle displaying the date and time
 function displayTime() {
-    var rightNow = moment().format('MMM Do YYYY')
-   var time = moment().hour('H')
-   timeDisplayEl.text(time)
-    dayDisplayEl.text(rightNow)
+    var rightNow = moment().format('MMM Do YYYY');
+   var time = moment().hour('H');
+   timeDisplayEl.text(time);
+    dayDisplayEl.text(rightNow);
 }
 setInterval(displayTime, 1000);
 
@@ -16,17 +16,17 @@ setInterval(displayTime, 1000);
 function checkTime() {
     var timeNow = moment().format('H');
     var timeBlockElements = $(".textarea");
-    var localStorageTime = window.localStorage.getItem('time') || {}
-    var parsedTime = JSON.parse(localStorageTime)
+    var localStorageTime = window.localStorage.getItem('time') || {};
+    var parsedTime = JSON.parse(localStorageTime);
 
     for (var i = 0; i < timeBlockElements.length; i++) {
         var elementID = timeBlockElements[i].id;
-        var manipID = document.getElementById(timeBlockElements[i].id)
-        var parent = $(timeBlockElements[i]).parent()
-        var dataTime = parent.attr('data-time')
-        var message = parsedTime[dataTime]
+        var manipID = document.getElementById(timeBlockElements[i].id);
+        var parent = $(timeBlockElements[i]).parent();
+        var dataTime = parent.attr('data-time');
+        var message = parsedTime[dataTime];
         if (message) {
-            $(timeBlockElements[i]).val(message)
+            $(timeBlockElements[i]).val(message);
         }
 
 
@@ -49,35 +49,21 @@ function handleFormSubmit (element) {
 
     var textarea = $(element).prev('.textarea');
 
-    console.log(textarea.val())
+    console.log(textarea.val());
 
-    var parent = $(element).parent()
-    var time = parent.attr('data-time')
-    var localStoragetime = window.localStorage.getItem('time')
-    var parseTime = {}
+    var parent = $(element).parent();
+    var time = parent.attr('data-time');
+    var localStoragetime = window.localStorage.getItem('time');
+    var parseTime = {};
     if(localStoragetime){
-        parseTime = JSON.parse(localStoragetime)
+        parseTime = JSON.parse(localStoragetime);
     } 
     
-    parseTime[time] = textarea.val()
+    parseTime[time] = textarea.val();
 
-    window.localStorage.setItem('time', JSON.stringify(parseTime))
+    window.localStorage.setItem('time', JSON.stringify(parseTime));
 
-    console.log(parent)
-    // var text =''
-  
-    // $("textarea.textarea").each(function(){
-    //         text += $(this).val().trim() + "\n";
-    //       })
-
-    // textArea.textContent = text;      
-    // localStorage.setItem('message', text);
-
-   //var messages = $('.textarea'); 
-   
-    
-    // text = savedVals
-    // textArea.textContent = text;
+    console.log(parent);
 }
 
 saveButtonEl.on('click', function (event) {
